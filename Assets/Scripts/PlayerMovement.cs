7a8c7a624090f998718bea1a74ffab54d0083ec8
nura-on <xnamespooferx@gmail.com>
2014-03-03 16:10:10 +0100

    missing files
    those files have been missin due to a wrong .gitignore file

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,13 @@ using System.Collections;
 /// </summary>
 public class PlayerMovement : Player {
 
-    private float _horizontal = 0;
-
 	// Use this for initialization
 	void Start () {
 	    
 	}
 	
 	// Update is called once per frame
-    void Update() {
-        _horizontal = Input.GetAxis("Horizontal");
-    }
-	void FixedUpdate () {
-        rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
-
-        if (_horizontal != 0) { 
-            this.rigidbody2D.AddForce(Vector2.right * _horizontal * 800f);
-        } 
+	void Update () {
+	
 	}
 }
