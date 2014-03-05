44ebdd0b8755bcf965bc110c7baf890315eea972
nura-on <xnamespooferx@gmail.com>
2014-03-05 17:13:16 +0100

    backup
    incomplete and not working

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        rigidbody2D.collisionDetectionMode = CollisionDetectionMode2D.None;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Destroyable")) {
+            Destroy(col.gameObject);
+        }
+    }
+}
