74d88970356c92d8a57edcac57edd65c9f944d65
Adrian Corpuz <adrian.corpuz@interadcorp.com>
2014-03-06 01:57:21 +0800

    Enemy Movement (added option to not follow player)

--- Assets/Scripts/RobotMovementHorizontal.cs
+++ Assets/Scripts/RobotMovementHorizontal.cs
@@ -6,9 +6,10 @@ public class RobotMovementHorizontal : MonoBehaviour {
 	private Player _player;
 	private bool _detectedPlayer = false;
 	private bool _stopForever = false;
-	private float _followPlayer;
+	private float _moveToPlayer;
 
 	public int _speed = 0; // We use inspector. For level design
+	public bool _followPlayer = true;
 
 	// Use this for initialization
 	void Start () {
@@ -35,13 +36,14 @@ public class RobotMovementHorizontal : MonoBehaviour {
 
 					// this is weird xD
 					float speed = 100; // milliseconds
-					if(_speed == 1) 		speed = 200;
+					if(_speed == 0)			speed = 0;
+					else if(_speed == 1) 	speed = 200;
 					else if(_speed == 2)	speed = 400;
 					else if(_speed == 3)	speed = 600;
 					else if(_speed == 4)	speed = 800;
 					else if(_speed == 5)	speed = 1000;
 
-					_followPlayer = (Time.time * 1000) + speed; 
+					_moveToPlayer = (Time.time * 1000) + speed; 
 				}
 				
 			}
@@ -52,13 +54,19 @@ public class RobotMovementHorizontal : MonoBehaviour {
 	{
 		if(_detectedPlayer)
 		{
+
 			int _followX = -2;
 			if(transform.position.x < _player.transform.position.x) _followX = 2;
+
+			if(!_followPlayer)
+			{
+				_followX = 0;
+			}
 
 			transform.position = new Vector2(transform.position.x + (_followX * Time.deltaTime), _player.transform.position.y - 10);
 			
 			// I like to use Time x) sorry
-			if(_followPlayer < Time.time * 1000)
+			if(_moveToPlayer < Time.time * 1000)
 			{
 				_detectedPlayer = false;
 				_stopForever = true;
