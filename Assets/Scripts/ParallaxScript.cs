dd840802703421aa0de36713eb0bd155b00bf676
Adrian Corpuz <drianade15@gmail.com>
2014-03-09 17:18:22 +0800

    Background pixel art and parallax.....

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -9,10 +9,21 @@ public class ParallaxScript : MonoBehaviour {
 	private float _zLayer = 100;
 	private float _moveSlight;
 	private bool _stopForever = false;
+
+	private float horizontal;
+
+	// BG: Uber fancy parallax x)
+	private Transform bg0, bg1, bg2,bg3;
+
 	
 	// Use this for initialization
 	void Start () {
 		_player = Player.Instance;
+
+		bg0 = transform.FindChild ("bg0");
+		bg1 = transform.FindChild ("bg1");
+		bg2 = transform.FindChild ("bg2");
+		bg3 = transform.FindChild ("bg3");
 	}
 	
 	// Update is called once per frame
@@ -26,7 +37,17 @@ public class ParallaxScript : MonoBehaviour {
 			{
 				_moveSlight = 14;
 			}
+
+
 			transform.position = new Vector3 (0, _player.transform.position.y - _setPosition + _moveSlight, _zLayer);
+			horizontal = Input.GetAxis("Horizontal") * 0.4f;
+			bg0.position = new Vector3 (0 - horizontal, _player.transform.position.y - _setPosition + _moveSlight - 1, _zLayer + 20);
+			horizontal = Input.GetAxis("Horizontal") * 0.3f;
+			bg1.position = new Vector3 (0 - horizontal, _player.transform.position.y - _setPosition + _moveSlight - 2, _zLayer + 19);
+			horizontal = Input.GetAxis("Horizontal") * 0.2f;
+			bg2.position = new Vector3 (0 - horizontal, _player.transform.position.y - _setPosition + _moveSlight - 3, _zLayer + 18);
+			horizontal = Input.GetAxis("Horizontal") * 0.1f;
+			bg3.position = new Vector3 (0 - horizontal, _player.transform.position.y - _setPosition + _moveSlight - 4, _zLayer + 17);
 		}
 	}
 }
