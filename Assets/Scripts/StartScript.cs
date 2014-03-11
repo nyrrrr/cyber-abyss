fd11f213240bd9e3c141a310eb4d1cdea0a0dca2
nura-on <xnamespooferx@gmail.com>
2014-03-11 20:16:59 +0100

    sound fix

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey) Application.LoadLevel("game");
+	}
+
+    void OnGUI() {
+        GUI.Label(new Rect(10, 10, 200, 200), "ENTER TITLE HERE");
+    }
+
+}
