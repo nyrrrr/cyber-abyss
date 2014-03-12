49057fcc9c3ef054f2a2a089ac5706a2f86b8f94
Adrian Corpuz <drianade15@gmail.com>
2014-03-12 09:08:24 +0800

    Moon changes

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -15,6 +15,7 @@ public class ParallaxScript : MonoBehaviour {
 	// BG: Uber fancy parallax x)
 	private Transform bg0, bg1, bg2,bg3, moon;
 
+
 	
 	// Use this for initialization
 	void Start () {
@@ -25,6 +26,7 @@ public class ParallaxScript : MonoBehaviour {
 		bg2 = transform.FindChild ("bg2");
 		bg3 = transform.FindChild ("bg3");
 		moon = transform.FindChild ("moon");
+
 	}
 	
 	// Update is called once per frame
@@ -42,7 +44,8 @@ public class ParallaxScript : MonoBehaviour {
 
 			transform.position = new Vector3 (0, _player.transform.position.y - _setPosition + _moveSlight, _zLayer);
 			horizontal = Input.GetAxis("Horizontal") * 0.6f;
-			moon.position = new Vector3 (Mathf.Lerp(moon.position.x, 0.1f - horizontal, Time.deltaTime), _player.transform.position.y + 2, _zLayer + 20);
+
+			moon.position = new Vector3 (Mathf.Lerp(moon.position.x, 0.1f - horizontal, Time.deltaTime), _player.transform.position.y - 5, _zLayer + 20);
 			horizontal = Input.GetAxis("Horizontal") * 0.4f;
             bg0.position = new Vector3(Mathf.Lerp(bg0.position.x, 0f - horizontal, Time.deltaTime), _player.transform.position.y - _setPosition + _moveSlight - 1, _zLayer + 20);
 			horizontal = Input.GetAxis("Horizontal") * 0.3f;
